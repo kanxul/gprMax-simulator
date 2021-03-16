d24eb978d102ad784035452043366f401425bd15
kanxul <kanxul@googlemail.com>
2021-03-16 16:01:55 +0100

    add image export

--- gprMax-simulator.py
+++ gprMax-simulator.py
@@ -9,6 +9,8 @@ from tools.plot_Bscan import get_output_data, mpl_plot
 
 import inspect
 
+from PIL import Image
+
 # Variables
 filename = 'cylinder_Bscan_2D'
 
@@ -28,6 +30,7 @@ def generate_Ascan_Bscan(filename):
 
 # Plot time-traces Radargramm
 def plot_radargramm(filename):
+    print("Plotting Radargramm")
 
     output_filepath = os.path.join('processing/', filename + '_merged.out')
 
@@ -35,9 +38,20 @@ def plot_radargramm(filename):
     rxcomponent = 'Ez'
     outputdata, dt = get_output_data(filename = output_filepath, rxnumber = rxnumber, rxcomponent = rxcomponent)
 
-    print(dt)
-    plt = mpl_plot(filename = output_filepath, outputdata = outputdata, dt = dt, rxnumber = rxnumber, rxcomponent = rxcomponent)
-    plt.show()
+    #plt = mpl_plot(filename = output_filepath, outputdata = outputdata, dt = dt, rxnumber = rxnumber, rxcomponent = rxcomponent)
+    #print(plt.savefig("test.png"))
+
+    print(outputdata)
+    save_filepath = os.path.join('processing/', filename + '.png')
+    print(save_filepath)
+    im = plt.imsave(save_filepath, outputdata, cmap = "seismic")
+
+    img = Image.open(save_filepath)
+    img.resize((240,210), Image.ANTIALIAS)
+    img.save("resized.png")
+
+    
+
 
 
 def main():
